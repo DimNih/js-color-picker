d3ae617279343016d9b116f658e0262a42c0b94a
DimNih <dd0613987@gmail.com>
2025-01-28 14:19:19 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# JS Color Picker
+
+## Deskripsi
+Ini adalah proyek **JS Color Picker** buat lo yang pengen pilih warna dengan mudah di website lo..
+
+## Fitur
+
+- Pilih warna dengan mudah
+- Support berbagai format warna (HEX, RGB, HSL)
+- Customizable sesuai kebutuhan 
---
+++ bekend.js
@@ -0,0 +1,25 @@
+const colorInput = document.getElementById('colorInput');
+    const colorDisplay = document.getElementById('colorDisplay');
+    const colorCode = document.getElementById('colorCode');
+    const colorOverlay = document.getElementById('colorOverlay');
+    const currentColor = document.getElementById('currentColor');
+
+    // Color Picker
+    colorInput.addEventListener('input', (event) => {
+      const selectedColor = event.target.value;
+      colorDisplay.style.backgroundColor = selectedColor;
+      colorOverlay.textContent = selectedColor;
+      currentColor.textContent = selectedColor;
+
+      // Glow container efek
+      colorDisplay.classList.add("glow");
+      setTimeout(() => {
+        colorDisplay.classList.remove("glow");
+      }, 500); 
+      colorDisplay.style.transform = "scale(1.05)";
+      setTimeout(() => {
+        colorDisplay.style.transform = "scale(1)";
+      }, 300); 
+
+
+    });
